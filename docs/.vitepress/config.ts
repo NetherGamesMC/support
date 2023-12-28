5a68ce82c96a0521a74984383a4a5f8d24092145
pnyyhz <callumdouglas3@icloud.com>
2023-12-28 15:29:50 +1100

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -19,9 +19,8 @@ export default {
             { text: 'In-game Emojis', link: '/in-game-emojis' },
             { text: 'Map Rotations', link: '/map-rotations' },
             { text: 'Useful Commands', link: '/useful-commands' },
-            { text: 'Season Rotations', link: '/season-rotations' }
+            { text: 'Season Rotations', link: '/season-rotations' },
             { text: 'Allocation of XP', link: '/allocation-of-xp' }
-
         ]
         },
         {
