b9382b75f65ff7af2d55246bbaae2552a0d1b041
pnyyhz <callumdouglas3@icloud.com>
2024-08-18 07:51:12 +0000

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -50,7 +50,7 @@ export default {
             { text: 'Reporting a Bug', link: '/reporting-a-bug' },
             { text: 'Reporting a Player', link: '/reporting-a-player' },
             { text: 'Submitting Feedback', link: '/submitting-feedback' },
-            { text: 'Syncing Ranks & Tiers to Discord', link: '/syncing-ranks-and-tiers-to-discord' },
+            { text: 'Linking Your NetherGames & Discord aAccount', link: '/linking-your-nethergames-and-discord-account' },
             { text: 'Titan Rank Questions', link: '/titan-perks-faq' },
             { text: 'Applying for a Refund', link: '/applying-for-a-refund' },
             { text: 'Managing Your Titan Subscription', link: '/managing-your-titan-subscription' }
